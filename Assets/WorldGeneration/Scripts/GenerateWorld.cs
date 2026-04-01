349cfe99de6842dda7ff42a3aca66bdbeb234c97
Jesse50gaming <128247303+Jesse50gaming@users.noreply.github.com>
2026-04-01 13:46:44 -0500

    s

--- Assets/WorldGeneration/Scripts/GenerateWorld.cs
+++ Assets/WorldGeneration/Scripts/GenerateWorld.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 public class GenerateWorld : MonoBehaviour
 {
     public GameObject chunkPrefab;
+    public GameObject player;
 
-    public int worldSize = 5; // 5x5 chunks
+    public int renderDistance = 16; // 16x16 by default
     public int chunkSize = 16;
 
     void Start()
@@ -12,11 +13,28 @@ public class GenerateWorld : MonoBehaviour
         generateWorld();
     }
 
+    void Update()
+    {
+        for (int x = 0; x < renderDistance; x++)
+        {
+            for (int z = 0; z < renderDistance; z++)
+            {
+                GameObject chunkObj = Instantiate(chunkPrefab);
+
+                chunkObj.transform.position = new Vector3(x * chunkSize, 0, z * chunkSize);
+
+                ChunkRenderer chunk = chunkObj.GetComponent<ChunkRenderer>();
+
+                chunk.Init(x * chunkSize, z * chunkSize);
+            }
+        }
+    }
+
     private void generateWorld()
     {
-        for (int x = 0; x < worldSize; x++)
+        for (int x = 0; x < renderDistance; x++)
         {
-            for (int z = 0; z < worldSize; z++)
+            for (int z = 0; z < renderDistance; z++)
             {
                 GameObject chunkObj = Instantiate(chunkPrefab);
 
